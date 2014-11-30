0a2c8d2ba84d0a0c8a5ad3e5996eeece76452ace
Sacha Bron <sacha.bron@copperflake.com>
2014-11-30 21:29:40 +0100

    Find Cycle

--- Labo 3/Sources/Lab 3/DirectedCycle.h
+++ Labo 3/Sources/Lab 3/DirectedCycle.h
@@ -3,7 +3,7 @@
 //  ASD2
 //
 //  Created by Olivier Cuisenaire on 08.10.14.
-//  
+//
 //  A implementer
 //  Classe permettant la detection de cycle sur un graphe oriente
 
@@ -11,33 +11,51 @@
 #define ASD2_DirectedCycle_h
 
 namespace ASD2 {
-    
     template<typename GraphType>
     class DirectedCycle {
     private:
-        /* A DEFINIR */
-        
-    public:
+        const GraphType& g;
+        bool hasCycle;
+        std::vector<bool> onStack;
+        std::vector<bool> marked;
 
-        //constructeur
+    public:
+        // constructor
         DirectedCycle(const GraphType& g) {
-            /* A IMPLEMENTER */
+            dfs(0);
         }
-        
+
+        void dfs(int v) {
+            onStack[v] = true;
+
+            for (int w: g.adjacent(v)) {
+                if (hasCycle) {
+                    return;
+                }
+
+                if (!marked[w]) {
+                    dfs(w);
+                } else if (onStack[w]) {
+                    hasCycle = true;
+                }
+            }
+
+            onStack[v] = false;
+        }
+
         //indique la presence d'un cycle
         bool HasCycle() {
-            /* A IMPLEMENTER */
-            //return ...
+            return hasCycle;
         }
-        
+
         //liste les indexes des sommets formant une boucle
         std::list<int> Cycle() {
             /* A IMPLEMENTER */
             //return ...
         }
-        
+
     };
-    
+
 }
 
 #endif
