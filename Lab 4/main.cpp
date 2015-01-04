fd9d90a92b1bf87a720536f4c744a1c27e6846cb
Sacha Bron <sacha.bron@copperflake.com>
2015-01-04 19:53:04 +0100

    Answers 1 and 2. Clear shortestPath

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -14,14 +14,35 @@
 #include "RoadNetwork.h"
 #include "MinimumSpanningTree.h"
 #include "ShortestPath.h"
+#include "RoadDiGraphWrapper.h"
 
 using namespace std;
 
 // Calcule et affiche le plus court chemin de la ville depart a la ville arrivee
 // en passant par le reseau routier rn. Le critere a optimiser est la distance.
 
-void PlusCourtChemin(const string& depart, const string& arrivee, RoadNetwork& rn) {   
-    
+void PlusCourtChemin(const string& depart, const string& arrivee, RoadNetwork& rn) {
+    int u = rn.cityIdx.at(depart);
+    int v = rn.cityIdx.at(arrivee);
+
+    RoadDiGraphWrapper rdgw(rn);
+    ASD2::DijkstraSP<RoadDiGraphWrapper> sp(rdgw, u);
+
+    std::vector<ASD2::WeightedDirectedEdge<double>> edges = sp.PathTo(v);
+
+    double totalLength = 0;
+
+    for (std::vector<ASD2::WeightedDirectedEdge<double>>::iterator edge = edges.begin(); edge != edges.end(); edge++) {
+        std::string from = rn.cities.at(edge->From()).name;
+        std::string to = rn.cities.at(edge->To()).name;
+        double len = edge->Weight();
+
+        totalLength += len;
+
+        std::cout << "De " << from << " à " << to << ": " << len << "km" << std::endl;
+    }
+
+    std::cout << "Distance totale: " << totalLength << std::endl << std::endl;
 }
 
 // Calcule et affiche le plus rapide chemin de la ville depart a la ville arrivee via la ville "via"
@@ -46,24 +67,24 @@ void ReseauLeMoinsCher(RoadNetwork &rn) {
 void testShortestPath(string filename)
 {
     cout << "Testing " << filename << endl;
-    
+
     bool ok = true;
-    
+
     typedef ASD2::EdgeWeightedDiGraph<double> Graph;
     Graph ewd(filename);
-    
+
     clock_t startTime = clock();
 
     ASD2::BellmanFordQueueSP<Graph> referenceSP(ewd,0);
-    
+
     cout << "Bellman-Ford: " << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << endl;
     startTime = clock();
 
     ASD2::DijkstraSP<Graph> testSP(ewd,0);
-    
+
     cout << "Dijkstra:     " << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << endl;
 
-    
+
     for (int v=0; v<ewd.V(); ++v) {
         if (referenceSP.DistanceTo(v) != testSP.DistanceTo(v) ) {
             cout << "Oops: vertex" << v << " has " << referenceSP.DistanceTo(v) << " != " <<  testSP.DistanceTo(v) << endl;
@@ -71,39 +92,39 @@ void testShortestPath(string filename)
             break;
         }
     }
-    
+
     if(ok) cout << " ... test succeeded " << endl << endl;
 }
 
 int main(int argc, const char * argv[]) {
-    
+
     testShortestPath("tinyEWD.txt");
     testShortestPath("mediumEWD.txt");
     testShortestPath("1000EWD.txt");
     testShortestPath("10000EWD.txt");
     //testShortestPath("largeEWD.txt"); // disponible sur dossier du cours
-    
+
     RoadNetwork rn("reseau.txt");
-    
-    cout << "1. Chemin le plus court entre Geneve et Emmen" << endl;
-    
+
+    cout << "1. Chemin le plus court entre Genève et Emmen:" << endl;
+
     PlusCourtChemin("Geneve", "Emmen", rn);
-    
-    cout << "2. Chemin le plus court entre Lausanne et Bale" << endl;
-    
+
+    cout << "2. Chemin le plus court entre Lausanne et Bâle:" << endl;
+
     PlusCourtChemin("Lausanne", "Basel", rn);
-  
-    cout << "3. chemin le plus rapide entre Geneve et Emmen en passant par Yverdon" << endl;
-    
+
+    cout << "3. Chemin le plus rapide entre Genève et Emmen en passant par Yverdon:" << endl;
+
     PlusRapideChemin("Geneve", "Emmen", "Yverdon-Les-Bains", rn);
-    
-    cout << "4. chemin le plus rapide entre Geneve et Emmen en passant par Vevey" << endl;
-    
+
+    cout << "4. Chemin le plus rapide entre Genève et Emmen en passant par Vevey:" << endl;
+
     PlusRapideChemin("Geneve", "Emmen", "Vevey", rn);
 
-    cout << "5. Quelles routes doivent etre renovees ? Quel sera le cout de la renovation de ces routes ?" << endl;
-    
+    cout << "5. Quelles routes doivent être renovées? Quel sera le coût de la rénovation de ces routes?" << endl;
+
     ReseauLeMoinsCher(rn);
-    
+
     return 0;
 }
