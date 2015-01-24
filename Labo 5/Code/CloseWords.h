4ac072413e221a0de0ff1e4245746923438ba857
Sacha Bron <sacha.bron@copperflake.com>
2015-01-24 18:30:53 +0100

    Unsorted set for CloseWords

--- Labo 5/Code/CloseWords.h
+++ Labo 5/Code/CloseWords.h
@@ -8,51 +8,56 @@
 #define CloeseWords_h
 
 #include <string>
-#include <list>
+#include <unordered_set>
 
 class CloseWords {
 public:
-    static std::list<std::string> getCloseWords(std::string str) {
-        std::list<std::string> words;
+    static std::unordered_set<std::string> getCloseWords(std::string str) {
+        std::unordered_set<std::string> words;
 
-        words.merge(getCloseWordsToMany(str));
-        words.merge(getCloseWordsMissing(str));
-        words.merge(getCloseWordsWrong(str));
-        words.merge(getCloseWordsSwap(str));
+        std::unordered_set<std::string> w1 = getCloseWordsToMany(str);
+        std::unordered_set<std::string> w2 = getCloseWordsMissing(str);
+        std::unordered_set<std::string> w3 = getCloseWordsWrong(str);
+        std::unordered_set<std::string> w4 = getCloseWordsSwap(str);
+
+        words.insert(w1.begin(), w1.end());
+        words.insert(w2.begin(), w2.end());
+        words.insert(w3.begin(), w3.end());
+        words.insert(w4.begin(), w4.end());
 
         return words;
     }
 
 private:
-    static std::list<std::string> getCloseWordsToMany(std::string str) {
-        std::list<std::string> words;
+    static std::unordered_set<std::string> getCloseWordsToMany(std::string str) {
+        std::unordered_set<std::string> words;
 
         for (int i = 0; i < str.size(); i++) {
             std::string tmp = str;
             tmp.erase(tmp.begin() + i);
-            words.push_back(tmp);
+            words.insert(tmp);
         }
 
         return words;
     }
 
-    static std::list<std::string> getCloseWordsMissing(std::string str) {
-        std::list<std::string> words;
+    static std::unordered_set<std::string> getCloseWordsMissing(std::string str) {
+        std::unordered_set<std::string> words;
         std::string alphabet = "abcdefghijklmnopqrstuvwxyz'";
 
         for (int i = 0; i < str.size() + 1; i++) {
             for(char c : alphabet) {
                 std::string tmp = str;
                 tmp.insert(tmp.begin() + i, c);
-                words.push_back(tmp);
+                words.insert(tmp);
             }
         }
 
         return words;
     }
 
-    static std::list<std::string> getCloseWordsWrong(std::string str) {
-        std::list<std::string> words;
+    static std::unordered_set<std::string> getCloseWordsWrong(std::string str) {
+        std::unordered_set<std::string> words;
         std::string alphabet = "abcdefghijklmnopqrstuvwxyz'";
 
         for (int i = 0; i < str.size(); i++) {
@@ -60,7 +65,7 @@ private:
                 if (str.at(i) != c) {
                     std::string tmp = str;
                     tmp.at(i) = c;
-                    words.push_back(tmp);
+                    words.insert(tmp);
                 }
             }
         }
@@ -68,8 +73,8 @@ private:
         return words;
     }
 
-    static std::list<std::string> getCloseWordsSwap(std::string str) {
-        std::list<std::string> words;
+    static std::unordered_set<std::string> getCloseWordsSwap(std::string str) {
+        std::unordered_set<std::string> words;
 
         for (int i = 0; i < str.size() - 1; i++) {
             std::string tmp = str;
@@ -79,7 +84,7 @@ private:
                 tmp.at(i) = tmp.at(i + 1);
                 tmp.at(i + 1) = c;
 
-                words.push_back(tmp);
+                words.insert(tmp);
             }
         }
 
--- Labo 5/main.cpp
+++ Labo 5/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <unordered_set>
 #include "Code/CloseWords.h"
 #include "Code/Dictionary.h"
 
@@ -11,11 +11,11 @@ int main()
 	Dictionary d("Data/dictionary.txt", 0);
 	std::cout << d.checkWord("hello") << std::endl;
 
-	std::list<std::string> words = CloseWords::getCloseWords("salut");
+    std::unordered_set<std::string> words = CloseWords::getCloseWords("salut");
 
-	for (std::list<std::string>::iterator word = words.begin(); word != words.end(); word++) {
-		std::cout << *word << std::endl;
-	}
+    for(std::unordered_set<std::string>::iterator word = words.begin(); word != words.end(); word++) {
+        std::cout << *word << std::endl;
+    }
 
 	return 0;
 }
