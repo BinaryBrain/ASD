b95d074a86a53bf607ecc101475e3aa934a4189c
Sacha Bron <sacha.bron@copperflake.com>
2015-01-24 18:21:18 +0100

    CloseWords

--- Labo 5/main.cpp
+++ Labo 5/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
+#include <list>
+#include "Code/CloseWords.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout << "Hello world!" << std::endl;
+    std::list<std::string> words = CloseWords::getCloseWords("salut");
+
+    for(std::list<std::string>::iterator word = words.begin(); word != words.end(); word++) {
+        std::cout << *word << std::endl;
+    }
+
     return 0;
 }
