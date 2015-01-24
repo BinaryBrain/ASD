1c6d127789186f7606cc3a7baaec2ae294865055
D34D10K <leonard.berney@epfl.ch>
2015-01-24 21:49:51 +0100

    first implementation of ternary search tree (untested)

--- Labo 5/Code/TernarySearchTrie.h
+++ Labo 5/Code/TernarySearchTrie.h
@@ -8,14 +8,77 @@ private:
 	struct Node {
 	public:
 		KeyType key;
-		ValueType value;
 		Node* right;
 		Node* left;
 		Node* middle;
 		int nodeSize;
-		Node(KeyType key, ValueType value) : key(key), value(value), right(nullptr), left(nullptr), middle(nullptr), nodeSize(1) { }
+		bool enOfWord;
+		Node(KeyType key) : key(key), enOfWord(false), right(nullptr), left(nullptr), middle(nullptr), nodeSize(1) { }
 	};
 
+	Node* root;
+
+public:
+	TernarySearchTrie() : root(nullptr){}
+
+
+	bool contains(const KeyType* key)
+	{
+		Node* x = root;
+		KeyType* k = key;
+
+		while (x != nullptr)
+		{
+			if (*k < x->key)
+			{
+				x = x->left;
+			}
+			else if (*k > x->key)
+			{
+				x = x->right;
+			}
+			else
+			{
+				if (*(k + 1) == nullptr)
+				{
+					return true;
+				}
+
+				k++;
+				x = x->middle;
+			}
+		}
+
+		return false;
+	}
+
+	void insert(Node* x, const KeyType* key)
+	{
+		if (x == nullptr)
+		{
+			*x = new Node(*key);
+		}
+
+		if (*(key) < (*x)->key)
+		{
+			insert(&((*x)->left), key);
+		}
+		else if (*(key) > (*x)->key)
+		{
+			insert(&((*x)->right), key);
+		}
+		else
+		{
+			if (*(x + 1))
+			{
+				insert(&((*x)->middle), key + 1);
+			}
+			else
+			{
+				(*x)->endOfWord = true;
+			}
+		}
+	}
 
 };
 
